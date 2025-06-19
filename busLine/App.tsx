3507bf910ba6e7f746d78a99f29e429e00462ed3
Munnawar23 <munawwarh48@gmail.com>
2025-06-19 23:13:50 +0530

    done

--- busLine/App.tsx
+++ busLine/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StatusBar, LogBox } from 'react-native';
+import { StatusBar} from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import './global.css';
 import Navigation from './src/navigation/Navigation';
